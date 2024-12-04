651b8262c185fc87323abfabf11e360c7fc5cf1a
wahyu.rizki <wahyu.rizki@accenture.com>
2024-12-04 13:27:18 +0700

    FEAT: illustration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,56 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="layoutWrapper full-height">
+      <header className="Header_header__U3aid">
+        <div className="Header_buttonContainerPlaceholder__rRZqY">
+          <div className="Header_buttonPlaceholder__U1v9p"></div>
+        </div>
+        <Image src="/logo.svg" alt="Next.js logo" width={124} height={30} priority />
+        <div className="Header_buttonContainer__pFjPm">
+          <div className="Header_button__Oae11">
+            <Image src="/refresh.svg" alt="Next.js logo" width={20} height={20} priority />
+          </div>
+        </div>
+      </header>
+      <main className="mainContent">
+        <div className="page_contentWrapper__YBAIW home">
+          <div className="page_background__odxEk page_home__jFQbK page_one__PgyQB"></div>
+          <div className="page_juicebotContainer__5Slx0">
+            <div className="container-visible visible">
+              <Image src="/illustration.svg" alt="Next.js logo" width={448} height={448} priority />
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="page_overlayTexts__hq_5I home">
+              <span className="page_topLeft__teS_m">
+                WA businesses feel confident about future growth
+              </span>
+              <span className="page_topRight__F61OY">AI can't replace creativity</span>
+              <span className="page_middleLeft__N_DHU">Sales measure true success</span>
+              <span className="page_middleRight__ii__n">Human connection drives WA business</span>
+              <span className="page_bottomLeft__f7V1K">
+                The primary barrier to digital transformation is financial investment
+              </span>
+            </div>
+          </div>
+          <div className="page_mainContent__51TpB">
+            <h1 className="page_mainText__9nGu9 home">
+              Compare your thoughts on <span className="page_highlight__KaGfy">technology</span>{" "}
+              with current industry opinions.
+            </h1>
+
+            <div className="page_buttonContainer__XCXUY home">
+              <div className="ChatBox_chatbotTextbox__yjSFM">
+                <div className="ChatBox_buttonWrapper__5ExBq">
+                  <button className="Button_button__0e1h7 Button_primary__lnswT">
+                    <span className="Button_buttonText__dTbaT">Get a reality check</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
